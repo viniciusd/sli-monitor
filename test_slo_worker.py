162cca33991eced5ed6ed7223195d3b5afeb8efc
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2017-06-25 17:53:29 -0300

    Adds initial test case

---
+++ test_slo_worker.py
@@ -0,0 +1,75 @@
+import unittest
+
+from slo_worker import  SloWorker
+
+class SloWorkerTest(unittest.TestCase):
+   
+    def setUp(self):
+        self.worker = SloWorker()
+
+    def test_do_request_returns_a_sequence(self):
+        urls = [
+                'http://www.fakeresponse.com/api?status=400',
+                ]
+
+        slos = self.worker.do_requests(urls)
+
+        self.assertNotIsInstance(slos, str)
+
+        with self.assertDoesNotRaise(TypeError):
+           _ = (e for e in my_object)
+
+    def test_do_request_when_no_urls_given(self):
+        urls = []
+        
+        slos = self.worker.do_requests(urls)
+
+        self.assertFalse(slos)
+        self.assertIsInstance(slos, list)
+
+    def test_do_request_when_status_code_is_400(self):
+        urls = [
+                'http://www.fakeresponse.com/api?status=400',
+                ]
+
+        slos = self.worker.do_requests(urls)
+
+        self.assertEqual(1, len(slos))
+        self.assertEqual(400, slos[0].status)
+
+
+    class _AssertDoesNotRaiseContext(unittest.case._AssertRaisesBaseContext):
+        """A context manager used to implement TestCase.assertRaises* methods.
+        """
+
+        _base_type = BaseException
+        _base_type_str = 'an exception type or tuple of exception types'
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc_value, tb):
+            if exc_type is None:
+                return True
+            if exc_type and issubclass(exc_type, self.expected):
+                try:
+                    exc_name = self.expected.__name__
+                except AttributeError:
+                    exc_name = str(self.expected)
+                if self.obj_name:
+                    self._raiseFailure("{} raised by {}".format(exc_name,
+                                                                self.obj_name))
+                else:
+                    self._raiseFailure("{} raised".format(exc_name))
+            else:
+                traceback.clear_frames(tb)
+            if not issubclass(exc_type, self.expected):
+                # let unexpected exceptions pass through
+                return False
+            # store exception, without traceback, for later retrieval
+            self.exception = exc_value.with_traceback(None)
+            return False
+
+    def assertDoesNotRaise(self, expected_exception, *args, **kwargs):
+        context = self._AssertDoesNotRaiseContext(expected_exception, self)
+        return context.handle('assertDoesNotRaise', args, kwargs) 
