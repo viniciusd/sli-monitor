57ef7dd0c034a68f93dcb73a743e3452ba28e14c
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2017-06-25 22:56:18 -0300

    Join instructions to assign variable only once

--- index.py
+++ index.py
@@ -9,11 +9,10 @@ db = utils.get_db_connection()
 
 @app.route('/')
 def index():
-    slos = utils.get_slos_from_db(db)
     slos = map(lambda x: (x[0], x[1], utils.is_fast_enough(x[0], x[1]),
                                 x[2], utils.is_successful_enough(x[0], x[2])
                           ),
-               slos
+               utils.get_slos_from_db(db)
                )
     return flask.render_template('index.html', slos=slos)
 
